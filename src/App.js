b7d0cb946c78b72d5e2e430598ec5c5a11afca6f
Tsokoloy Manka <tsokoloy@gmail.com>
2020-04-12 20:21:50 +0800

    Author quiz structure and css completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './bootstrap.min.css';
 
-function App() {
+
+  function Hero(){
+    return (
+      <div className="row">
+        <div className="jumbotron col-10 offset-1">
+          <h1>Author Quiz</h1>
+          <p>Select the book written by the author shown</p>
+        </div>
+      </div>
+    )
+  }
+
+  function Turn({author, books, highlight}){
+
+    //choose wh
+    function highlightBG(highlight){
+      const mapping = {
+        'none': '',
+        'correct': 'green',
+        'wrong' : 'red'
+      }
+      return mapping[highlight];
+
+    }
+    return (
+      <div className="row turn" style={{backgroundColor: highlightBG(highlight)}}> 
+        <div className="col-4 offset-1">
+            <img src={author.imageUrl} className="authorimage" alt="Author"/>
+        </div>
+        <div className="col-6">
+          {books.map((title) => <Book title={title} key={title}/>)}
+        </div>
+      </div>
+    );
+  }
+
+  function Book ({title}){
+    return (
+      <div className="answer">
+        <h4>{title}</h4>
+      </div>
+    )
+  }
+
+
+  function Continue() {
+    return (
+      <div></div>
+    )
+  }
+
+  function Footer(){
+    return (
+      <div id="footer" className="row">
+        <div className="text-muted credit">All images are from <a href="https://commons.wikimedia.org/">Wikemedia Common</a></div>
+      </div>
+    )
+  }
+
+function App({ turnData, highlight }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid'> 
+        <div className="container-fluid">
+        <Hero />
+        <Turn {...turnData} highlight={highlight}/>
+        <Continue />
+        <Footer />
+        </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,87 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore'
+
+const authors = [
+
+  //use require to read that this link is an image
+  {
+    name: 'Mark Twain',
+    imageUrl: require('./images/authors/marktwain.jpg'),
+    imageSource: 'Wikemedia Commons',
+    books: [
+      'The Adventure of Huckleberry Finn',
+      'Life on the Mississipi',
+      'Roughing'  
+    ]
+  },
+  {
+    name: 'Jospeh Conrad',
+    imageUrl: require('./images/authors/josephconrad.jpg'),
+    imageSource: 'Wikimedia Commons',
+    books: ['Heart of Darkness']
+  },
+  {
+    name: 'J.K Rowling',
+    imageUrl: require('./images/authors/jkrowling.jpg'),
+    imageSource: 'Wikimedia Commons',
+    imageAttribution: 'Daniel Ogren',
+    books: ['Harry Potter and the Sorcerer stone']
+  },
+  {
+    name: 'Stephen King',
+    imageUrl: require('./images/authors/stephenking.jpg'),
+    imageSource: 'Wikimedia Commons',
+    imageAttribution: 'Pinguino',
+    books: ['the shining', 'IT']
+  },
+  {
+    name: 'Charles Dickens',
+    imageUrl: require('./images/authors/charlesdickens.png'),
+    imageSource: 'Wikimedia Commons',
+    books: ['David Copperfield', 'A tale of Two Cities']
+  },
+  {
+    name: 'William Shakespeare',
+    imageUrl: require('./images/authors/williamshakespear.jpg'),
+    imageSource: 'Wikimedia Commons',
+    books: ['Hamlet', 'Macbeth', 'Romeo and Juliet ']
+  }
+];
+
+//data na e send sa main app.js, which is array sya: compose of array of data
+const state = {
+  //pass to a function
+  turnData: getTurnData(authors), //array ni sya
+  highlight: 'correct'
+};
+
+
+function getTurnData(authors){
+  //slice array to pick only the books row/column
+  const allBooks = authors.reduce(function (p,c,i ){
+    return p.concat(c.books);
+  }, []);
+
+  //picking up the 4 random books
+  const fourRandomBooks = shuffle(allBooks).slice(0,4);
+
+  //ang answer sa 4 ka random pick, is stored
+  const answer = sample(fourRandomBooks);
+
+  return {
+    books: fourRandomBooks,
+    
+    author: authors.find((author) => 
+      author.books.some((title) => title === answer)
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App {...state}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
